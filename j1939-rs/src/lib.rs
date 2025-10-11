57a2d53bcd57d74e459079b7e6b14d2029b452d3
cojmeister <luqas.c@gmail.com>
2025-10-11 12:03:00 +0300

    Added the facade lib

--- j1939-rs/src/lib.rs
+++ j1939-rs/src/lib.rs
@@ -1,14 +1,9 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
-}
+#![no_std]
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub use j1939_core::*;
+pub use j1939_macros::*;
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub mod prelude {
+    pub use j1939_core::*;
+    pub use j1939_macros::*;
 }
